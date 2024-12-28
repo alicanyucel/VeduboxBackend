d289c79cb2cb80dfdf97f24a8faee5e4c909026d
Ali Can Yücel (The Coderman) <yucelalican@hotmail.com>
2024-12-28 15:00:03 +0300

    Add project files.

---
+++ VeduboxBackend/VeduboxBackend.Application/Services/IJwtProvider.cs
@@ -0,0 +1,10 @@
+﻿using VeduboxBackend.Application.Features.Auth.Login;
+using VeduboxBackend.Domain.Entities;
+
+namespace VeduboxBackend.Application.Services
+{
+    public interface IJwtProvider
+    {
+        Task<LoginCommandResponse> CreateToken(AppUser user);
+    }
+}
---
+++ VeduboxBackend/VeduboxBackend.Infrastructure/Configurations/AppUserConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using VeduboxBackend.Domain.Entities;
+
+namespace VeduboxBackend.Infrastructure.Configurations
+{
+    internal sealed class AppUserConfiguration : IEntityTypeConfiguration<AppUser>
+    {
+        public void Configure(EntityTypeBuilder<AppUser> builder)
+        {
+            builder.Property(p => p.FirstName).HasColumnType("varchar(50)");
+            builder.Property(p => p.LastName).HasColumnType("varchar(50)");
+        }
+    }
+}
